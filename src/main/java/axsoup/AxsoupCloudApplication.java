0b5ed01f49ab1682da21daa70006e58222336214
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-09 01:03:41 +0300

    Создана исходная структура проекта. Написан класс контроллера\ для обработки запроса домашней страницы. Определен шаблон представления для отображения домашней страницы. Написан простой тестовый класс для домашней страницы.

---
+++ src/main/java/axsoup/AxsoupCloudApplication.java
@@ -0,0 +1,12 @@
+package axsoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxsoupCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxsoupCloudApplication.class, args);
+	}
+}
