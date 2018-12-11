f996600015bdddc3e0be95fe9e6973006cca5b4e
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-11 18:25:09 +0300

    В контроллервх указал, что проверка должна выполняться, когда формы размещены в соответствующих методах-обработчиках.

--- src/main/java/axsoup/web/OrderController.java
+++ src/main/java/axsoup/web/OrderController.java
@@ -2,11 +2,14 @@ package axsoup.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import axsoup.Order;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -17,8 +20,12 @@ public class OrderController {
         model.addAttribute("order", new Order());
         return "orderForm";
     }
+
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
