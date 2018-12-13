f64a7ad7a23dffe94a1167ad6ffc956468d6e501
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-13 16:45:07 +0300

    Создал несколько запросов в репозиторий Заказов

--- src/main/java/axsoup/data/OrderRepository.java
+++ src/main/java/axsoup/data/OrderRepository.java
@@ -1,8 +1,27 @@
 package axsoup.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import axsoup.Order;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> findByDeliveryZip(String deliveryZip);
+
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
+    List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(
+            String deliveryTo, String deliveryCity);
+
+// to order by the deliveryTo
+    List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
+
+    @Query("Order o where o.deliveryCity='Moskow'")
+    List<Order> readOrdersDeliveredInSeattle();
+
 
 }
