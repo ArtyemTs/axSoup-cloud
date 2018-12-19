d1c0633f6f2c1931183ef4f5015fb79f3c7b1765
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-19 18:15:24 +0300

    Добавил Autowired с репозиториями в контроллер заказа. Все полностью работает!

--- src/main/java/axsoup/web/OrderController.java
+++ src/main/java/axsoup/web/OrderController.java
@@ -3,6 +3,8 @@ package axsoup.web;
 import javax.validation.Valid;
 
 
+import axsoup.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -16,29 +18,37 @@ import axsoup.Order;
 import axsoup.User;
 import axsoup.data.OrderRepository;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
 
+@Autowired
     private OrderRepository orderRepo;
 
-    public OrderController(OrderRepository orderRepo) {
+    private UserRepository userRepo;
+
+    public OrderController(OrderRepository orderRepo, UserRepository userRepo) {
         this.orderRepo = orderRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/current")
-    public String orderForm(@AuthenticationPrincipal User user) {
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
     public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
-                               @AuthenticationPrincipal User user) {
+                               Principal principal) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        User user = userRepo.findByUsername(
+                principal.getName());
         order.setUser(user);
 
         orderRepo.save(order);
